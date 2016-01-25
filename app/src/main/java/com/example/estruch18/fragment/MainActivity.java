7df479dfea66664066bba33ebe090ea542420c41
estruch18 <estruch95.b@gmail.com>
2016-01-25 22:25:29 +0100

    UPDATE

--- app/src/main/java/com/example/estruch18/fragment/MainActivity.java
+++ app/src/main/java/com/example/estruch18/fragment/MainActivity.java
@@ -4,13 +4,99 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageSwitcher imageSwitcher;
+    private ArrayList<Integer> imagenes;
+    private int position;
+    private static final Integer DURATION = 2500;
+
+    private Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cargaImagenes();
+        imageSwitcher = (ImageSwitcher)findViewById(R.id.imageSwitcher);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                return new ImageView(MainActivity.this);
+            }
+        });
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.abc_fade_in);
+        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.abc_fade_out);
+        imageSwitcher.setInAnimation(fadeIn);
+        imageSwitcher.setOutAnimation(fadeOut);
+
+        View v = null;
+        start(v);
+    }
+
+    public void start(View button) {
+        if (timer != null) {
+            timer.cancel();
+        }
+        position = 0;
+        startSlider();
+    }
+
+    public void stop(View button) {
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+    public void startSlider() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+            public void run() {
+                // avoid exception:
+                // "Only the original thread that created a view hierarchy can touch its views"
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        imageSwitcher.setImageResource(imagenes.get(position));
+                        position++;
+                        if (position == imagenes.size()) {
+                            position = 0;
+                        }
+                    }
+                });
+            }
+
+        }, 0, DURATION);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (timer != null) {
+            startSlider();
+        }
+
     }
 
     @Override
@@ -33,5 +119,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void cargaImagenes(){
+        imagenes = new ArrayList<Integer>();
+        imagenes.add(R.drawable.fondo1);
+        imagenes.add(R.drawable.fondo2);
+        imagenes.add(R.drawable.fondo3);
+        imagenes.add(R.drawable.fondo4);
+        imagenes.add(R.drawable.fondo5);
     }
 }
